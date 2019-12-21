156a55f117d29c3560e7ac6e8386c56a8e4f63fa
ansonfoong <afoong@albany.edu>
2019-12-21 18:18:18 -0500

    Created a single pokemon type component that will display the type dynamically with custom colors and css

---
+++ src/app/components/pokemon-type/pokemon-type.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-type',
+  templateUrl: './pokemon-type.component.html',
+  styleUrls: ['./pokemon-type.component.css']
+})
+export class PokemonTypeComponent implements OnInit {
+
+  @Input() type: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.type)
+  }
+
+}

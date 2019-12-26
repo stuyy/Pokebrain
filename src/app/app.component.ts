50dfa4cfd43e2ff9b764adb2b94f51a392fbd943
ansonfoong <afoong@albany.edu>
2019-12-26 06:57:21 -0500

    Added select form to differentiate pokemon, type, ability, etc. searches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   title = 'pokedex';
   public pokemonInput: string;
   public pokemonData: Pokemon;
+  public searchType: string = "pokemon";
   public success: boolean = false;
   public errorOccured: boolean = false;
   public errorMessage: string;
@@ -18,6 +19,7 @@ export class AppComponent {
 
   }
   getPokemon() {
+    console.log(this.searchType);
     this.success = false;
     this.loading = true;
     this.pokemon.getPokemon(this.pokemonInput)
@@ -32,6 +34,7 @@ export class AppComponent {
       this.success = false;
       this.errorOccured = true;
       this.errorMessage = err.error.msg;
+      this.loading = false;
     }, () => {
       setTimeout(() => { this.loading = false }, 1000)
     });
